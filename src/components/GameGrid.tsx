714123779baaffd13e2de00f2b0b86b08332d9e8
Chris Howell <monostation1@gmail.com>
2024-07-29 15:28:56 +0100

    Create a generic data fetching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,7 +5,7 @@ import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames(); // Getting games from custom hook!
+  const { data, error, isLoading } = useGames(); // Getting games from custom hook!
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
@@ -22,7 +22,7 @@ const GameGrid = () => {
               <GameCardSkeleton key={skeleton} />
             </GameCardContainer>
           ))}
-        {games.map((game) => (
+        {data.map((game) => (
           <GameCardContainer key={game.id}>
             <GameCard key={game.id} game={game}></GameCard>
           </GameCardContainer>
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,43 +1,10 @@
-import { CanceledError } from "axios";
-import apiClient from "../services/api-client";
-import { useEffect, useState } from "react";
+import useData from "./useData";
 
-interface Genre {
+export interface Genre {
   id: number;
   name: string;
-  //slug: string;
-  // image_background:string;
 }
 
-interface FetchGenresResponse {
-  count: number;
-  results: Genre[];
-}
-
-const useGenres = () => {
-  const abortController = new AbortController();
-  const [genres, setGenres] = useState<Genre[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    apiClient
-      .get<FetchGenresResponse>("/genres")
-      .then((res) => {
-        setGenres(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err);
-        setLoading(false);
-      });
-
-    return () => abortController.abort();
-  }, []);
-
-  return { genres, error, isLoading };
-};
+const useGenres = () => useData<Genre>("/genres");
 
 export default useGenres;
