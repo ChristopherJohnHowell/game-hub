460984a24ca2ee4a8e83d685d6d267df9582e34a
Chris Howell <monostation1@gmail.com>
2024-07-30 14:18:25 +0100

    Build seach input

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,6 @@ export interface GameQuery {
 function App() {
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
 
-  console.log(gameQuery);
-
   return (
     <Grid
       templateAreas={{
