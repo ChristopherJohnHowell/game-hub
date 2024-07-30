70f1ea315074637ce64c99cc34b7b2169adfd1c9
Chris Howell <monostation1@gmail.com>
2024-07-30 12:03:02 +0100

    Refactor: Extract a game query object

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,12 +1,4 @@
-import {
-  Card,
-  CardBody,
-  Heading,
-  HStack,
-  Icon,
-  Image,
-  Text,
-} from "@chakra-ui/react";
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,6 @@
 const getCroppedImageUrl = (url: string) => {
   const target = "media/";
+  if (!url) return;
   const index = url.indexOf(target) + target.length;
   const newURL = url.slice(0, index) + "crop/600/400/" + url.slice(index);
   return newURL;
